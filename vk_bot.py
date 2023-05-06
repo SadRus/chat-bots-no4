afe09a2053a2d513891d75dd4df3e3df7c88a54d
SadRus <rystixx@yandex.ru>
2023-05-06 23:59:48 +0300

    Change button colors

--- vk_bot.py
+++ vk_bot.py
@@ -14,10 +14,10 @@ from quiz import create_quiz
 
 def create_keyboard():
     keyboard = VkKeyboard()
-    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
     keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
     keyboard.add_line()
-    keyboard.add_button('Мой счет', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Мой счет')
     return keyboard
 
 
